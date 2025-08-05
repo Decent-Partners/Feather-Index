949d1278c610f017623011650f87cc2d7fe04954
Jonathan Brown <jbrown@acuity.network>
2025-08-05 18:33:23 +0700

    Open db.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::process::exit;
+use std::{path::PathBuf, process::exit};
 
 use clap::{
     Parser,
@@ -9,6 +9,7 @@ use clap::{
 };
 use clap_verbosity_flag::{InfoLevel, Verbosity};
 use color_eyre::eyre::Result;
+use sled::Tree;
 use subxt::{
     OnlineClient, PolkadotConfig, backend::rpc::RpcClient, ext::subxt_rpcs::LegacyRpcMethods,
 };
@@ -32,11 +33,30 @@ pub fn get_styles() -> Styles {
 #[derive(Parser, Debug, Clone)]
 #[command(author, version, about, long_about = None, styles=get_styles())]
 pub struct Args {
+    /// Database path
+    #[arg(short, long)]
+    pub db_path: Option<String>,
     /// URL of Substrate node to connect to
     #[arg(short, long)]
     pub url: Option<String>,
     #[command(flatten)]
     verbose: Verbosity<InfoLevel>,
+}
+
+/// Database trees for the indexer
+#[derive(Clone)]
+pub struct Trees {
+    pub root: sled::Db,
+    pub span: Tree,
+}
+
+pub fn open_trees(db_config: sled::Config) -> Result<Trees, sled::Error> {
+    let db = db_config.open()?;
+    let trees = Trees {
+        root: db.clone(),
+        span: db.open_tree(b"span")?,
+    };
+    Ok(trees)
 }
 
 #[tokio::main]
@@ -46,6 +66,30 @@ async fn main() -> Result<()> {
     let args: Args = Args::parse();
     let log_level = args.verbose.log_level_filter().as_trace();
     tracing_subscriber::fmt().with_max_level(log_level).init();
+    // Open database.
+    let db_path = match args.db_path {
+        Some(db_path) => PathBuf::from(db_path),
+        None => match home::home_dir() {
+            Some(mut db_path) => {
+                db_path.push(".local/share/feather-index/");
+                db_path.push("db");
+                db_path
+            }
+            None => {
+                error!("No home directory.");
+                exit(1);
+            }
+        },
+    };
+    info!("Database path: {}", db_path.display());
+    let db_config = sled::Config::new().path(db_path);
+    let trees = match open_trees(db_config) {
+        Ok(trees) => trees,
+        Err(_) => {
+            error!("Failed to open database.");
+            exit(1);
+        }
+    };
     // Determine url of Substrate node to connect to.
     let url = match args.url {
         Some(url) => url,
