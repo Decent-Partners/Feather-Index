a67a9388597ab419375c046b3683175fe15ce47e
Jonathan Brown <jbrown@acuity.network>
2025-08-05 18:07:00 +0700

    Connect to Kusama node.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::process::exit;
+
 use clap::{
     Parser,
     builder::{
@@ -7,7 +9,13 @@ use clap::{
 };
 use clap_verbosity_flag::{InfoLevel, Verbosity};
 use color_eyre::eyre::Result;
-use tracing_log::AsTrace;
+use subxt::{
+    OnlineClient, PolkadotConfig, backend::rpc::RpcClient, ext::subxt_rpcs::LegacyRpcMethods,
+};
+use tracing_log::{
+    AsTrace,
+    log::{error, info},
+};
 
 // https://github.com/rust-lang/cargo/blob/master/src/cargo/util/style.rs
 pub fn get_styles() -> Styles {
@@ -24,6 +32,9 @@ pub fn get_styles() -> Styles {
 #[derive(Parser, Debug, Clone)]
 #[command(author, version, about, long_about = None, styles=get_styles())]
 pub struct Args {
+    /// URL of Substrate node to connect to
+    #[arg(short, long)]
+    pub url: Option<String>,
     #[command(flatten)]
     verbose: Verbosity<InfoLevel>,
 }
@@ -35,5 +46,26 @@ async fn main() -> Result<()> {
     let args: Args = Args::parse();
     let log_level = args.verbose.log_level_filter().as_trace();
     tracing_subscriber::fmt().with_max_level(log_level).init();
+    // Determine url of Substrate node to connect to.
+    let url = match args.url {
+        Some(url) => url,
+        None => "wss://kusama-rpc.polkadot.io:443".into(),
+    };
+    info!("Connecting to: {}", url);
+    let rpc_client = match RpcClient::from_url(&url).await {
+        Ok(rpc_client) => rpc_client,
+        Err(err) => {
+            error!("Failed to connect: {}", err);
+            exit(1);
+        }
+    };
+    let api = match OnlineClient::<PolkadotConfig>::from_rpc_client(rpc_client.clone()).await {
+        Ok(api) => api,
+        Err(err) => {
+            error!("Failed to connect: {}", err);
+            exit(1);
+        }
+    };
+    let rpc = LegacyRpcMethods::<PolkadotConfig>::new(rpc_client);
     Ok(())
 }
